e87350adf6fbf12e3a8821fc591c668fb440d3ac
Rob Wettlaufer <robert.wettlaufer@synacor.com>
2017-12-29 13:39:28 -0800

    Day 14, Part 2

--- Day14 - Disk Defragmentation/main.go
+++ Day14 - Disk Defragmentation/main.go
@@ -14,50 +14,197 @@ func main() {
 	partOne(inputHash)
 	partTwo(inputHash)
 }
+
+/*
+ ____            _     _____
+|  _ \ __ _ _ __| |_  |_   _|_      _____
+| |_) / _` | '__| __|   | | \ \ /\ / / _ \
+|  __/ (_| | |  | |_    | |  \ V  V / (_) |
+|_|   \__,_|_|   \__|   |_|   \_/\_/ \___/
+*/
 func partTwo(input string) {
-	var groupNo = 1
-	var table diskMap
+	var groupNo int
+	table := createTable(input)
+	for i := 0; i < len(table); i++ {
+		for ii := 0; ii < len(table[i]); ii++ {
+			nGroup := table.getNeighborGroup(i, ii)
+			if nGroup != -1 {
+				table.floodFill(i, ii, nGroup)
+			} else if table.getValue(i, ii) == 0 && table.get(i, ii) == "*" {
+				groupNo++
+				table.floodFill(i, ii, groupNo)
+			}
+		}
+	}
+
+	fmt.Println(groupNo)
+}
+
+func createTable(input string) rows {
+	var t []string
 	for i := 0; i < 128; i++ {
 		hash := helpers.KnotHash(fmt.Sprintf("%s-%d", input, i))
 		var binH string
 		for _, c := range hash {
 			binH += HexToBin(string(c))
 		}
-		table = append(table, strings.Replace(binH, "1", "*", -1))
+		t = append(t, strings.Replace(binH, "1", "*", -1))
+	}
+	var res rows
+	for _, line := range t {
+		var r row
+		for i := 0; i < len(line); i++ {
+			r = append(r, cell{
+				display: string(line[i]),
+				set:     string(line[i]) != "*",
+			})
+		}
+		res = append(res, r)
 	}
 
-	for i := 0; i < len(table); i++ {
-		for ii := 0; i < len(table[i]); ii++ {
-			if table.get(i, ii) == "*" {
-				// Mark self
-				n := table.getNeighborGroup(i, ii)
-				if n != -1 {
-					table.set(i, ii, strconv.Itoa(n))
-					continue
-				}
-				table.set(i, ii, strconv.Itoa(groupNo))
+	return res
+}
 
-				// Mark Above
-				if i != 0 && table.get(i-1, ii) == "*" {
-					table.set(i-1, ii, strconv.Itoa(groupNo))
-				}
-				// Mark Right
-				if ii+1 < len(table[i]) && table.get(i, ii+1) == "*" {
-					table.set(i, ii+1, strconv.Itoa(groupNo))
-				}
-				// Mark Left
-				if ii-1 >= 0 && table.get(i, ii-1) == "*" {
-					table.set(i, ii-1, strconv.Itoa(groupNo))
-				}
-				// Mark Below
-				if i+1 < len(table) && table.get(i+1, ii) == "*" {
-					table.set(i+1, ii, strconv.Itoa(groupNo))
-				}
-				groupNo++
+type rows []row
+type row []cell
+type cell struct {
+	display string
+	group   int
+	set     bool
+}
+
+func (r *rows) set(rowIdx, lineIdx, groupNo int) {
+	(*r)[rowIdx][lineIdx].group = groupNo
+	(*r)[rowIdx][lineIdx].set = true
+}
+
+func (r *rows) getValue(row, col int) int {
+	if row > len((*r)) || col >= len((*r)[row]) {
+		return -1
+	}
+
+	return (*r)[row][col].group
+}
+
+func (r *rows) get(row, col int) string {
+	if row > len((*r)) || col > len((*r)[row]) {
+		return ""
+	}
+
+	display := (*r)[row][col].display
+	return display
+}
+
+func (r *rows) floodFill(row, col, groupNo int) {
+	if (*r)[row][col].set {
+		return
+	}
+
+	var check = func(r *rows, row, col int) bool {
+		return !(*r)[row][col].set
+	}
+
+	(*r)[row][col].group = groupNo
+	(*r)[row][col].set = true
+
+	// Check Below
+	if row+1 < len((*r)) && check(r, row+1, col) {
+		r.floodFill(row+1, col, groupNo)
+	}
+
+	// Check Right
+	if col+1 < len((*r)[row]) && check(r, row, col+1) {
+		r.floodFill(row, col+1, groupNo)
+	}
+
+	// Check Left
+	if col-1 >= 0 && check(r, row, col-1) {
+		r.floodFill(row, col-1, groupNo)
+	}
+
+	// Check Up
+	if row-1 >= 0 && check(r, row-1, col) {
+		r.floodFill(row-1, col, groupNo)
+	}
+}
+
+func (r *rows) getNeighborGroup(i, ii int) int {
+	var check = func(r *rows, row, col int) bool {
+		return r.getValue(row, col) != 0
+	}
+
+	// Check Self (We may have been set previously)
+	if check(r, i, ii) {
+		return r.getValue(i, ii)
+	}
+
+	// Check Above
+	if i != 0 && check(r, i-1, ii) {
+		return r.getValue(i-1, ii)
+	}
+	// Check Right
+	if ii+1 < len((*r)[i]) && check(r, i, ii+1) {
+		return r.getValue(i, ii+1)
+	}
+
+	// Check Left
+	if ii-1 >= 0 && check(r, i, ii-1) {
+		return r.getValue(i, ii-1)
+	}
+	// Check Below
+	if i+1 < len((*r)) && check(r, i+1, ii) {
+		return r.getValue(i+1, ii)
+	}
+
+	return -1
+}
+
+/*
+ ____            _      ___
+|  _ \ __ _ _ __| |_   / _ \ _ __   ___
+| |_) / _` | '__| __| | | | | '_ \ / _ \
+|  __/ (_| | |  | |_  | |_| | | | |  __/
+|_|   \__,_|_|   \__|  \___/|_| |_|\___|
+*/
+func partOne(input string) {
+	var rows []string
+	for i := 0; i < 128; i++ {
+		hash := helpers.KnotHash(fmt.Sprintf("%s-%d", input, i))
+		var binH string
+		for _, c := range hash {
+			binH += HexToBin(string(c))
+		}
+		rows = append(rows, binH)
+	}
+
+	var count int
+	for _, line := range rows {
+		for _, c := range line {
+			if string(c) == "1" {
+				count++
 			}
 		}
 	}
-	fmt.Println(groupNo)
+
+	fmt.Println(count)
+}
+
+type diskMap []string
+
+func (d *diskMap) set(rowIdx, lineIdx int, groupNo string) {
+	(*d)[rowIdx] = (*d)[rowIdx][:lineIdx] + groupNo + (*d)[rowIdx][lineIdx+1:]
+}
+
+func (d *diskMap) get(row, col int) string {
+	if row > len((*d)) || col > len((*d)[row]) {
+		return ""
+	}
+
+	return string((*d)[row][col])
+}
+
+func markBitAsGroup(table []string, rowIdx, lineIdx, groupNo int) {
+	table[rowIdx] = table[rowIdx][:lineIdx] + strconv.Itoa(groupNo) + table[rowIdx][lineIdx+1:]
 }
 
 func (d *diskMap) getNeighborGroup(i, ii int) int {
@@ -89,47 +236,6 @@ func (d *diskMap) getNeighborGroup(i, ii int) int {
 	}
 
 	return -1
-}
-
-func (d *diskMap) set(rowIdx, lineIdx int, groupNo string) {
-	(*d)[rowIdx] = (*d)[rowIdx][:lineIdx] + groupNo + (*d)[rowIdx][lineIdx+1:]
-}
-
-func (d *diskMap) get(row, col int) string {
-	if row > len((*d)) || col > len((*d)[row]) {
-		return ""
-	}
-
-	return string((*d)[row][col])
-}
-
-func markBitAsGroup(table []string, rowIdx, lineIdx, groupNo int) {
-	table[rowIdx] = table[rowIdx][:lineIdx] + strconv.Itoa(groupNo) + table[rowIdx][lineIdx+1:]
-}
-
-type diskMap []string
-
-func partOne(input string) {
-	var rows []string
-	for i := 0; i < 128; i++ {
-		hash := helpers.KnotHash(fmt.Sprintf("%s-%d", input, i))
-		var binH string
-		for _, c := range hash {
-			binH += HexToBin(string(c))
-		}
-		rows = append(rows, binH)
-	}
-
-	var count int
-	for _, line := range rows {
-		for _, c := range line {
-			if string(c) == "1" {
-				count++
-			}
-		}
-	}
-
-	fmt.Println(count)
 }
 
 func HexToBin(hex string) string {
